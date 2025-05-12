0382794b88754c9734f1575a70aa47bdfff70aac
SebaasG <miguel.poveda1220@gmail.com>
2025-05-12 07:23:37 -0500

    feat: :sparkles: Creacion de conexion correcta

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using sgi_app.domain.factory;
+using sgi_app.infrastructure.mysql;
+
+namespace CslApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var basePath = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", ".."));
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var dbSettings = configuration.GetSection("DatabaseSettings");
+            
+            var server = dbSettings["Server"];
+            var port = dbSettings["Port"];
+            var database = dbSettings["Database"];
+            var user = dbSettings["User"];
+            var password = dbSettings["Password"];
+
+            var connectionString = $"Server={server};Port={port};Database={database};User={user};Password={password};";
+
+            Console.WriteLine(connectionString);
+
+            IDbFactory factory = new mysqlDbFactory(connectionString);
+
+        }
+    }
+}
---
+++ infrastructure/mysql/mysqlDbFactory.cs
@@ -0,0 +1,23 @@
+using MySql.Data.MySqlClient;
+using sgi_app.domain.factory;
+
+
+namespace sgi_app.infrastructure.mysql
+{
+    public class mysqlDbFactory : IDbFactory               
+    {
+        private readonly string _connectionString;
+
+        public mysqlDbFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+            ObtenerConexion();
+        }
+
+    
+        public MySqlConnection ObtenerConexion()
+        {
+            return ConexionSingleton.Instancia(_connectionString).ObtenerConexion();
+        }
+    }
+}
